b04591614fd81037e9cc346b2c576ded719c1c0f
AgenceRevontuli <tristan@revontuli.fr>
2023-12-11 17:06:55 +0100

    🎨 Add style && front to the openAI response

--- resources/js/Pages/NewPost.vue
+++ resources/js/Pages/NewPost.vue
@@ -2,17 +2,41 @@
   <div>
     <h1>Créer une publication</h1>
   </div>
-  <div class="block-auth">
-    <form @submit.prevent="getOpenAIResponse">
-      <div class="input-auth">
-        <label for="subject">Vous pensez à un sujet ? </label>
-        <input v-model="prompt" type="text" id="subject" name="subject" />
+  <div class="flex gap-8 justify-between">
+    <div class="bg-neutral-50 bg-opacity-20 rounded-lg border-2 border-gray-200 w-6/12 p-16 mb-4">
+      <form @submit.prevent="getOpenAIResponse">
+        <div class="input-auth">
+          <label for="subject">Vous pensez à un sujet ? </label>
+          <input v-model="prompt" type="text" id="subject" name="subject" />
+        </div>
+        <button class="btn">Écrire une publication</button>
+      </form>
+    </div>
+    <div class="rounded-lg bg-white border border-gray-200 w-6/12 p-6">
+      <div class="flex gap-4 items-center">
+        <div
+          class="bg-gray-600 rounded-full border border-gray-200 w-[60px] h-[60px] flex justify-center items-center text-white"
+        >
+          TT
+        </div>
+        <div>
+          <h3>
+            {{
+              !$page.props.auth.user
+                ? 'Anonyme'
+                : $page.props.auth.user.first_name + ' ' + $page.props.auth.user.last_name
+            }}
+            <span class="text-gray-400 text-xs">● 1er</span>
+          </h3>
+          <p class="text-sm text-gray-400">Ceci est ta super baseline</p>
+        </div>
       </div>
-      <button class="btn">Écrire une publication</button>
-    </form>
-  </div>
-  <div v-if="openAIResponse && openAIResponse.choices && openAIResponse.choices.length > 0">
-    <p>Réponse d'OpenAI: {{ openAIResponse.choices[0].text }}</p>
+      <div class="mt-6">
+        <p v-if="openAIResponse && openAIResponse.choices && openAIResponse.choices.length > 0">
+          {{ openAIResponse.choices[0].text }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
