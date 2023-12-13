3a7a509e5646d3b463ff6c9e5b1486110496f8a9
AgenceRevontuli <tristan@revontuli.fr>
2023-12-13 19:00:16 +0100

    🎨 Style: Change font & add Tailwind Theme's colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ['./resources/**/*.{edge,js,ts,vue,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Satoshi-Regular', 'sans-serif'],
+    },
+    fontSize: {
+      '2xs': '0.625rem',
+      'xs': '0.75rem',
+      'sm': '0.775rem',
+      'base': '1rem',
+      'md': '1.125rem',
+      'lg': '1.25rem',
+      'xl': '1.5rem',
+      '2xl': '2rem',
+      '3xl': '2.5rem',
+      '4xl': '3rem',
+      '5xl': '3.5rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+      '8xl': '6rem',
+      '9xl': '7rem',
+    },
+    extend: {
+      colors: {
+        primary: '#F27B50',
+        secondary: '#F2B872',
+        tertiary: '#F2CFC2',
+        decor: '#F2F2F2',
+        dark: '#0D0D0D',
+      },
+    },
   },
   plugins: [],
 }
